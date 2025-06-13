f792bbb2e907589d42b9b1b4021121bd313f90b0
SakuraGaChiru <cfy070406@gmail.com>
2025-06-13 23:05:04 +0800

    add RainbowAnimation form UnoCSS

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,22 +1,68 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import {h,watch} from 'vue'
+import type { Theme } from 'vitepress'
+
+//customTheme
+import CSSLayout from './components/CSSLayout.vue'
+let homePageStyle: HTMLStyleElement | undefined
+import './style/index.css'
 import 'uno.css'
-import './style.css'
-import "vitepress-markdown-timeline/dist/theme/index.css";
-import CopyRight from './components/CopyRight.vue';
-import NonCopyRight from './components/NonCopyRight.vue';
-
-
+//import vue
+import RainbowAnimationSwitcher from "./components/RainbowAnimationSwitcher.vue";
+import CopyRight from "./components/CopyRight.vue";
+import NonCopyRight from "./components/NonCopyRight.vue";
+//config
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-    })
+    return h(CSSLayout)
   },
-  enhanceApp({ app, router, siteData }) {
-      app.component('CopyRight', CopyRight)
-      app.component('NonCopyRight', NonCopyRight)
+
+  enhanceApp({app , router }) {
+    app.component('RainbowAnimationSwitcher', RainbowAnimationSwitcher)
+    app.component('CopyRight', CopyRight)
+    app.component('NonCopyRight', NonCopyRight)
+    // 彩虹背景动画样式
+    if (typeof window === 'undefined')
+      return
+
+    watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true },
+    )
+  },
+}satisfies Theme
+
+// 彩虹背景动画样式
+if (typeof window !== 'undefined') {
+  // detect browser, add to class for conditional styling
+  const browser = navigator.userAgent.toLowerCase()
+  if (browser.includes('chrome'))
+    document.documentElement.classList.add('browser-chrome')
+  else if (browser.includes('firefox'))
+    document.documentElement.classList.add('browser-firefox')
+  else if (browser.includes('safari'))
+    document.documentElement.classList.add('browser-safari')
+}
+// Speed up the rainbow animation on home page
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle)
+      return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
   }
-} satisfies Theme
+  else {
+    if (!homePageStyle)
+      return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
+  }
+}
