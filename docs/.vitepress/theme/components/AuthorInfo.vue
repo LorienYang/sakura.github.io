1facd6d9c155bbbd4e78be6fc84902bf144c0c37
SakuraGaChiru <cfy070406@gmail.com>
2025-06-17 20:48:30 +0800

    新增作者信息组件

---
+++ docs/.vitepress/theme/components/AuthorInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="author-info">
+    <p class="author-name">作者：{{ name }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.author-info {
+  margin-top: 2rem;
+  padding-top: 1rem;
+  border-top: 1px solid var(--vp-c-divider);
+  text-align: right;
+  font-size: 0.9em;
+  color: var(--vp-c-text-2);
+}
+</style>
